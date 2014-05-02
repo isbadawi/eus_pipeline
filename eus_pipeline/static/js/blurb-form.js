0ec3dfbd5bfeb65d5cd95940d8b958f7fa0d10e3
Ismail Badawi <ismail@badawi.io>
2014-05-02 01:34:58 -0400

    Simplify blurb form JS.
    (Start using Knockout.)

--- eus_pipeline/static/js/blurb-form.js
+++ eus_pipeline/static/js/blurb-form.js
@@ -1,26 +1,18 @@
+var pipeline = pipeline || {};
+
+pipeline.BlurbFormViewModel = function() {
+  var self = this;
+  self.attachments = ko.observable(0);
+
+  self.attachments.subscribe(function (newValue) {
+    $('#id_document_set-TOTAL_FORMS').val(newValue);
+  });
+
+  self.addAttachment = function() {
+    self.attachments(self.attachments() + 1);
+  };
+};
+
 $(function() {
-    $('body').data('attachmentid', 0);
-    $('li.required label').each(function() {
-        $(this).html($(this).html() + ' <span style="color: red;">*</span>');
-    });
-    $('#more-attachments').click(function(e) {
-        e.preventDefault();
-        var id = $('body').data('attachmentid');
-        $('body').data('attachmentid', id + 1);
-        var newfield = $('<ul>');
-        newfield.append('<li>What is it? <br/>\n');
-        var title = $('<input type="text" maxlength="50">');
-        title.attr('id', 'id_document_set-' + id + '-title');
-        title.attr('name', 'document_set-' + id + '-title');
-        newfield.append(title);
-        newfield.append('\n</li>\n');
-        var file = $('<input type="file">');
-        file.attr('id', 'id_document_set-' + id + '-data');
-        file.attr('name', 'document_set-' + id + '-data');
-        newfield.append('<li>');
-        newfield.append(file);
-        newfield.append('</li>');
-        $('#attachment-list').append(newfield);
-        $('#id_document_set-TOTAL_FORMS').val(id + 1);
-    });
-}); 
+  ko.applyBindings(new pipeline.BlurbFormViewModel());
+});
